82923f13f20ef74a3a2490f258d3d18ee03282ea
Jenny Song <jenny.jieun.song@gmail.com>
2015-01-23 21:08:41 -0800

    first commit

--- WhatDoWeDoNow/GameScene.swift
+++ WhatDoWeDoNow/GameScene.swift
@@ -9,37 +9,66 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let sport01 = SKSpriteNode(imageNamed: "sport01")
+    let sport02 = SKSpriteNode(imageNamed: "sport02")
+    let sport03 = SKSpriteNode(imageNamed: "sport03")
+    let sport04 = SKSpriteNode(imageNamed: "sport04")
+    let sport05 = SKSpriteNode(imageNamed: "sport05")
+    let sport06 = SKSpriteNode(imageNamed: "sport06")
+    let sport07 = SKSpriteNode(imageNamed: "sport07")
+    let sport08 = SKSpriteNode(imageNamed: "sport08")
+    let sport09 = SKSpriteNode(imageNamed: "sport09")
+    let sport10 = SKSpriteNode(imageNamed: "sport10")
+    let sport11 = SKSpriteNode(imageNamed: "sport11")
+    let sport12 = SKSpriteNode(imageNamed: "sport12")
+    let sport13 = SKSpriteNode(imageNamed: "sport13")
+    let sport14 = SKSpriteNode(imageNamed: "sport14")
+    let sport15 = SKSpriteNode(imageNamed: "sport15")
+
+
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        backgroundColor = SKColor.whiteColor()
+        sport01.position = CGPoint(x: size.width * 0.1, y: size.height * 0.2)
+        sport02.position = CGPoint(x: size.width * 0.3, y: size.height * 0.2)
+        sport03.position = CGPoint(x: size.width * 0.5, y: size.height * 0.2)
+        sport04.position = CGPoint(x: size.width * 0.7, y: size.height * 0.2)
+        sport05.position = CGPoint(x: size.width * 0.9, y: size.height * 0.2)
+        sport06.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        sport07.position = CGPoint(x: size.width * 0.3, y: size.height * 0.5)
+        sport08.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
+        sport09.position = CGPoint(x: size.width * 0.7, y: size.height * 0.5)
+        sport10.position = CGPoint(x: size.width * 0.9, y: size.height * 0.5)
+        sport11.position = CGPoint(x: size.width * 0.1, y: size.height * 0.8)
+        sport12.position = CGPoint(x: size.width * 0.3, y: size.height * 0.8)
+        sport13.position = CGPoint(x: size.width * 0.5, y: size.height * 0.8)
+        sport14.position = CGPoint(x: size.width * 0.7, y: size.height * 0.8)
+        sport15.position = CGPoint(x: size.width * 0.9, y: size.height * 0.8)
+        addChild(sport01)
+        addChild(sport02)
+        addChild(sport03)
+        addChild(sport04)
+        addChild(sport05)
+        addChild(sport06)
+        addChild(sport07)
+        addChild(sport08)
+        addChild(sport09)
+        addChild(sport10)
+        addChild(sport11)
+        addChild(sport12)
+        addChild(sport13)
+        addChild(sport14)
+        addChild(sport15)
         
-        self.addChild(myLabel)
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+    func override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        <#code#>
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    
+    func override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        <#code#>
     }
+    
 }
