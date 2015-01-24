709caeb886e2b02e964fd34f35a5d6cd9ee922db
Jenny Song <jenny.jieun.song@gmail.com>
2015-01-23 23:55:30 -0800

    fixed Action button and scene

--- WhatDoWeDoNow/ActionButton.swift
+++ WhatDoWeDoNow/ActionButton.swift
@@ -27,6 +27,7 @@ class ActionButton: SKNode {
         addChild(activeButton)
     }
 
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
